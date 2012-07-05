b1c09854d0b9c67bf8a94d3334fccdbf031ef373
daniele <daniele@w085.dnmed.cf.ac.uk>
2012-07-05 15:31:03 +0100

    Django 1.4 project structure

--- arkestra_medic/settings.py
+++ arkestra_medic/settings.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# Django settings for arkestra_medic project.
 
 from sekrit_settings import *
 
@@ -7,17 +7,25 @@ gettext = lambda s: s
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
-# Django settings for arkestra_medic project.
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-                       
+
 ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
 )
 
 MANAGERS = ADMINS
 
-
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': '',                      # Or path to database file if using sqlite3.
+#       'USER': '',                      # Not used with sqlite3.
+#       'PASSWORD': '',                  # Not used with sqlite3.
+#       'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#       'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#   }
+#}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -26,13 +34,11 @@ MANAGERS = ADMINS
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/London'
-DATE_FORMAT = "jS F Y"
-TIME_FORMAT = "H\.i"
+TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
@@ -41,11 +47,13 @@ SITE_ID = 1
 USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
+# calendars according to the current locale.
 USE_L10N = True
 
-BASE_PATH = os.path.normpath(os.path.dirname(__file__))
+# If you set this to False, Django will not use timezone-aware datetimes.
+# USE_TZ = True
 
+BASE_PATH = os.path.normpath(os.path.dirname(__file__))
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
@@ -83,15 +91,17 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'legacy_finders.LegacyAppDirectoriesFinder',    
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
+# Make this unique, and don't share it with anybody.
+# SECRET_KEY = '7g8^r53fr1xsen(2qzolj-z1k%(1#+8w(5e(84zh8btw*-*z6n'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -101,32 +111,11 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     'django.core.context_processors.static',
-    # 'django.core.context_processors.csrf',
     'django.contrib.messages.context_processors.messages',
-
     "cms.context_processors.media",
     'sekizai.context_processors.sekizai',
-
     "arkestra_utilities.context_processors.arkestra_templates",
 )
-
-MIDDLEWARE_CLASSES = (
-
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-
-
-
-    # 'cms.middleware.multilingual.MultilingualURLMiddleware',
-    'cms.middleware.page.CurrentPageMiddleware',
-    'cms.middleware.user.CurrentUserMiddleware',
-    'cms.middleware.toolbar.ToolbarMiddleware',    
-
-
-    )
 
 THUMBNAIL_PROCESSORS = (
     'easy_thumbnails.processors.colorspace',
@@ -135,7 +124,25 @@ THUMBNAIL_PROCESSORS = (
     'easy_thumbnails.processors.filters',
     )
 
-ROOT_URLCONF = 'urls'
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # 'cms.middleware.multilingual.MultilingualURLMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',    
+)
+
+ROOT_URLCONF = 'arkestra_medic.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'arkestra_medic.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -215,6 +222,7 @@ CMS_LANGUAGES = (
 ('cy', gettext('Cymraeg')),
 )
 
+# CMS_MODERATOR = False
 INSTALLED_APPS = (
 
      # Django CMS applications
@@ -265,20 +273,25 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.humanize',
-    'django.contrib.staticfiles',
 )
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
+# the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
